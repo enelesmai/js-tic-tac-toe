3bf7c94f001a542a1e1116397b91d9b7c49d5c7e
enelesmai <xochitl.selene.fp@gmail.com>
2020-08-03 15:33:14 -0500

    added tests to gamelogic and gameboard modules

--- src/js/gamelogic.test.js
+++ src/js/gamelogic.test.js
@@ -4,6 +4,7 @@ import { GameLogic } from './gamelogic';
 const w1 = ['6', '4', '2'];
 const w2 = ['6', '1', '2', '4'];
 let p1 = Player('p1', 'X');
+let p2 = Player('p2', 'O');
 
 
 /* Testing winner move */
@@ -18,4 +19,13 @@ test('it verifies no winner sequence', () => {
     expect(GameLogic.winnerMove(w2)).toBe(true);
 });
 
+test('it verifies the game starts if players have different names', () => {
+    expect(GameLogic.startMatch(p1, p2)).toBe(true);
+});
 
+test('it switches the current player', () => {
+    GameLogic.startMatch(p1, p2);
+    GameLogic.setCurrentPlayer(p1);
+    let player = GameLogic.switchCurrentPlayer();
+    expect(player.name).toBe(p2.name);
+});
--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,29 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/layout-tictactoe.css'
 import '../css/style.css'
-
 import { GameLogic } from './gamelogic';
+import { Player } from './player';
+import { DisplayController } from './displaycontroller';
+import { Gameboard } from './gameboard';
+
+let p1;
+let p2;
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('warningMessage').classList.add('hide');
-  document.getElementById('startButton').addEventListener('click', () => {
-    GameLogic.startMatch();
-  });
-  document.getElementById('restartButton').addEventListener('click', () => {
-    GameLogic.restartGame();
-  });
+    document.getElementById('warningMessage').classList.add('hide');
+    document.getElementById('startButton').addEventListener('click', () => {
+        p1 = Player(document.getElementById('inputPlayer1').value, 'X');
+        p2 = Player(document.getElementById('inputPlayer2').value, 'O');
+        var start = GameLogic.startMatch(p1, p2);
+        if (start) {
+            DisplayController.prepareGameScreen();
+            DisplayController.updateGame(Gameboard.getBoardArray(), [p1, p2]);
+            DisplayController.renderNextMove();
+        } else {
+            DisplayController.showWarningMessage(errors);
+        }
+    });
+    document.getElementById('restartButton').addEventListener('click', () => {
+        GameLogic.restartGame();
+    });
 });
